16dab284ac8a09158b19b02d2dd8836b59217705
vaporbird <ivyan98@gmail.com>
2023-12-07 17:35:00 +0200

    Do infinite turn right bug fix

--- 006_functions_code_block_while_loops/006_reeborg.ca_maze.py
+++ 006_functions_code_block_while_loops/006_reeborg.ca_maze.py
@@ -1,17 +1,25 @@
 #https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
 def turn_right():
-    for i in range(0,3):
+    for i in range(0, 3):
         turn_left()
 
+preventInfinite = 0
 while not at_goal():
     if right_is_clear():
         turn_right()
         move()
+        preventInfinite += 1
+        if preventInfinite >= 4:
+            if front_is_clear():
+                preventInfinite = 0
+                move()
     elif front_is_clear():
+        preventInfinite = 0
         move()
     else:
         while not front_is_clear():
             turn_left()
+            preventInfinite = 0
         move()
 
 
